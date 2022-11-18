c29190ac1e2cbe48f47375d2225c0b7e81fb15a4
brams <63436162+ibndiaye@users.noreply.github.com>
2022-11-18 23:36:13 +0100

    added kitty, fish, neofetch

---
+++ config/fish/config.fish
@@ -0,0 +1,10 @@
+set fish_greeting
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+#fastfetch
+neofetch
+colorscript exec blocks1
+alias unlock='sudo rm /var/lib/pacman/db.lck'
+alias tmpats='gamemoderun mangohud truckersmp-cli --disable-steamruntime --rendering-backend dx11 --proton --game-options="-nointro" -vv start atsmp'
+alias tmpets='gamemoderun mangohud truckersmp-cli --disable-steamruntime --rendering-backend dx11 --proton --game-options="-nointro" -vv start ets2mp'

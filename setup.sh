3fafeb654c8ccc0637a5a78ac985b2d3f30e0efd
brams <63436162+ibndiaye@users.noreply.github.com>
2022-12-02 20:44:48 +0100

    working

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-
-mydots="$HOME/Documents/GitHub/dotfiles/config"
+mkdir "$HOME/Documents/GitHub/"
+mydots="$HOME/Documents/GitHub/"
 CONFIG_PATH="$HOME/.config/"
 
 if ! command -v yay &> /dev/null
@@ -14,7 +14,7 @@ then
     # makepkg -si
 else
     echo "installing packages..."
-    yay -S flameshot-git git neofetch nerd-fonts-source-code-pro nitrogen nvidia-dkms nvidia-settings spotify-adblock-git timeshift timeshift-autosnap tldr grub-btrfs dmenu grub-btrfs rofi
+    yay -S flatpak flameshot-git git neofetch nerd-fonts-source-code-pro nitrogen nvidia-dkms nvidia-settings spotify-adblock-git timeshift timeshift-autosnap tldr grub-btrfs dmenu grub-btrfs rofi
     
 fi
 
@@ -27,5 +27,11 @@ mv dotfiles $mydots
 
 cd $mydots
 ln -s $mydots/cava $CONFIG_PATH
+ln -s $mydots/fish $CONFIG_PATH
+ln -s $mydots/rofi $CONFIG_PATH
+ln -s $mydots/neofetch $CONFIG_PATH
+ln -s $mydots/kitty $CONFIG_PATH
+ln -s $mydots/i3 $CONFIG_PATH
+ln -s $mydots/MangoHud $CONFIG_PATH
  
 

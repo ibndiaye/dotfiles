dfc950ce4278d5ebe2b92473a7f821d8d112b315
brams <63436162+ibndiaye@users.noreply.github.com>
2022-12-02 21:05:30 +0100

    change how the script is built

--- setup.sh
+++ setup.sh
@@ -4,21 +4,6 @@ mkdir "$HOME/Documents/GitHub/"
 mydots="$HOME/Documents/GitHub/"
 CONFIG_PATH="$HOME/.config/"
 
-if ! command -v yay &> /dev/null
-then
-    echo "yay could not be found, installing yay..."
-    sudo pacman -Syyu yay-bin
-    # cd && pacman -S --needed git base-devel
-    # git clone https://aur.archlinux.org/yay.git
-    # cd yay
-    # makepkg -si
-else
-    echo "installing packages..."
-    yay -S flatpak flameshot-git git neofetch nerd-fonts-source-code-pro nitrogen nvidia-dkms nvidia-settings spotify-adblock-git timeshift timeshift-autosnap tldr grub-btrfs dmenu grub-btrfs rofi
-    
-fi
-
-
 # clone dots
 echo "cloning git repo - dotfiles"
 cd && git clone https://github.com/ibndiaye/dotfiles
@@ -34,4 +19,19 @@ ln -s $mydots/kitty $CONFIG_PATH
 ln -s $mydots/i3 $CONFIG_PATH
 ln -s $mydots/MangoHud $CONFIG_PATH
  
+
+
+if ! command -v yay &> /dev/null
+then
+    echo "yay could not be found, installing yay..."
+    sudo pacman -Syyu yay-bin
+    # cd && pacman -S --needed git base-devel
+    # git clone https://aur.archlinux.org/yay.git
+    # cd yay
+    # makepkg -si
+else
+    echo "installing packages..."
+    yay -S flatpak flameshot-git git neofetch nerd-fonts-source-code-pro nitrogen nvidia-dkms nvidia-settings spotify-adblock-git timeshift timeshift-autosnap tldr grub-btrfs dmenu grub-btrfs rofi
+    
+fi
 

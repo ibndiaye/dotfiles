be842df95be29ab9ae912e97adfef77cd48a9df1
brams <63436162+ibndiaye@users.noreply.github.com>
2022-12-02 18:19:16 +0100

    upload of configs and creation of bash script

---
+++ backup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#listNames=("cava"  "conky" "fish" "rofi" "nvim" "neofetch" "kitty" "i3" "picom")
+
+printf "Backing up..."
+
+CONFIG_PATHS="$HOME/.config"
+WP_PATH="$HOME/Pictures/wallpapers/"
+
+
+CONFIG_SAVE_PATH="$HOME/Documents/GitHub/dotfiles/config/"
+WP_SAVE_PATH="$HOME/Documents/GitHub/dotfiles/wallpapers"
+
+#backup configs
+cp -r "$CONFIG_PATHS/conky" $CONFIG_SAVE_PATH
+cp -r "$CONFIG_PATHS/cava" $CONFIG_SAVE_PATH
+cp -r "$CONFIG_PATHS/fish" $CONFIG_SAVE_PATH
+cp -r "$CONFIG_PATHS/rofi" $CONFIG_SAVE_PATH
+sudo cp -r "$CONFIG_PATHS/nvim" $CONFIG_SAVE_PATH
+cp -r "$CONFIG_PATHS/neofetch" $CONFIG_SAVE_PATH
+cp -r "$CONFIG_PATHS/kitty" $CONFIG_SAVE_PATH
+cp -r "$CONFIG_PATHS/i3" $CONFIG_SAVE_PATH
+cp -r "$CONFIG_PATHS/picom.conf" $CONFIG_SAVE_PATH
+cp -r "$CONFIG_PATHS/MangoHud" $CONFIG_SAVE_PATH
+
+#backup wp
+cp -r "$WP_PATH/" $WP_SAVE_PATH
+
+
+printf "everythings backed up"
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,10 +1,42 @@
-set fish_greeting
+#########################
+##variables
+########################
+set EDITOR vim
+set fish_greeting 
+
+##################
+#####path
+#################
+
+fish_add_path ~/.local/bin
+#fish_add_path ~/.doom.d
+fish_add_path ~/usr/local/bin
+##########################################
+###########     aliases     ##############
+#########################################
+#alias nvim='lvim'
+#alias emacs='emacsclient -c -a 'emacs''
+#alis nv='nvim'
+alias mkex='chmod +x'
+alias smi='sudo make install'
+alias els='exa -l --color=always --group-directories-first --icons'
+alias la='exa -al --color=always --group-directories-first --icons'
+alias l.='exa -a | rg "^\."'
+alias gitc='git clone'
+alias rr='curl -s -L https://raw.githubusercontent.com/keroserene/rickrollrc/master/roll.sh | bash'
+alias stx="startx"
+alias pm="sudo pacman"
+########################################
+########################################
+
+##############################################
+############startup#######################
+#############################################
+fish_vi_key_bindings
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-#fastfetch
+starship init fish | source
+
 neofetch
-colorscript exec blocks1
-alias unlock='sudo rm /var/lib/pacman/db.lck'
-alias tmpats='gamemoderun mangohud truckersmp-cli --disable-steamruntime --rendering-backend dx11 --proton --game-options="-nointro" -vv start atsmp'
-alias tmpets='gamemoderun mangohud truckersmp-cli --disable-steamruntime --rendering-backend dx11 --proton --game-options="-nointro" -vv start ets2mp'
+#alsi

9b56d13484b39a1640f8ee6f17c6f0c8d707893f
brams <63436162+ibndiaye@users.noreply.github.com>
2023-04-01 10:13:21 +0200

    add qtile

---
+++ config/qtile/autostart.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#feh --bg-scale /usr/share/endeavouros/backgrounds/endeavouros-wallpaper.png
+picom  & # --experimental-backends --vsync should prevent screen tearing on most setups if needed
+
+# Low battery notifier
+#~/.config/qtile/scripts/check_battery.sh & disown
+
+# Start welcome
+#eos-welcome & disown
+
+greenclip daemon &
+flameshot &
+nitrogen --restore &
+
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & disown # start polkit agent from GNOME

d05d62a7eb28da649e03c90786dfcd41ed7490ee
brams <63436162+ibndiaye@users.noreply.github.com>
2023-05-05 20:36:27 +0200

    config change

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -9,6 +9,7 @@ set fish_greeting
 #################
 
 fish_add_path ~/.local/bin
+fish_add_path ~/.bin
 #fish_add_path ~/.doom.d
 fish_add_path ~/usr/local/bin
 ##########################################
@@ -40,10 +41,10 @@ fish_vi_key_bindings
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-starship init fish | source
+#starship init fish | source
 
 neofetch
 alias tmpats='gamemoderun mangohud truckersmp-cli --disable-steamruntime --rendering-backend dx11 --proton --game-options="-nointro" -vv start atsmp'
 alias tmpets='gamemoderun mangohud truckersmp-cli --disable-steamruntime --rendering-backend dx11 --proton --game-options="-nointro" -vv start ets2mp'
-
+alias kssh='kitty +kitten ssh'
 #alsi
